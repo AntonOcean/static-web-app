9ad64b647f5cc156398561f4dabc7e5642a6a44e
AntonOcean <tmath90@yandex.ru>
2019-09-29 13:12:21 +0300

    Чтение файла - отдельной функцией

--- src/views.py
+++ src/views.py
@@ -9,6 +9,11 @@ from http_engine.response import parse_http_response
 ROOT_DIR = CONFIG.get('document_root', '/var/www/html')
 
 
+def get_file(path):
+    with open(path, "rb") as fs:
+        return fs.read()
+
+
 def static_handler(request):
     if request["method"] not in [GET, HEAD]:
         return parse_http_response(request, status=status.MethodNotAllowed)
@@ -29,7 +34,6 @@ def static_handler(request):
     if not os.path.exists(abs_path) or not os.path.isfile(abs_path):
         return parse_http_response(request, status=status.NotFound)
 
-    with open(abs_path, "rb") as fs:
-        content = fs.read()
+    content = get_file(abs_path)
 
     return parse_http_response(request, content=content)
